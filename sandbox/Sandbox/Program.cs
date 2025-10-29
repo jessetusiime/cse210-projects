baf8350c3ac79583eef56cfb6a0c5b1b40d6e764
jessetusiime <jessetusiime078@gmail.com>
2025-10-29 14:35:30 +0300

    Fininshed first exercise

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is Jesse's first C# program!");
+        Console.WriteLine("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.Write($"Your favorite color is {color}");
+
     }
 }
