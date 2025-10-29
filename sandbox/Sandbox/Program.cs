ff48878a50fa364c01c857daf7fa428d71f7a8c8
jessetusiime <jessetusiime078@gmail.com>
2025-10-29 17:18:30 +0300

    Completed exercise 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,11 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
-        Console.WriteLine("This is Jesse's first C# program!");
-        Console.WriteLine("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.Write($"Your favorite color is {color}");
+        // Console.WriteLine("Hello World! This is the Sandbox Project.");
+        // Console.WriteLine("This is Jesse's first C# program!");
+        // Console.WriteLine("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.Write($"Your favorite color is {color}");
 
+        int x = 5;
+        int y = 2;
+
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("Equal");
+        }
+        
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int mark = int.Parse(input);
+
+        string grade = "";
+
+        if (mark >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (mark >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (mark >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (mark >= 60)
+        {
+            grade = "D";
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (mark >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry! Next try time try harder!");
+        }
     }
 }
