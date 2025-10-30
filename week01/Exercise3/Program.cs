d67e5c42a05b02b9a8f16aac24267b5ff652d4ed
jessetusiime <jessetusiime078@gmail.com>
2025-10-30 11:53:15 +0300

    Completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        guess = int.Parse(userGuess);
+
+        if (guess > number)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (guess < number)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it");
+        }
+    }    
     }
 }
