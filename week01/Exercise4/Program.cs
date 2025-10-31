b4db79067b5e7b728d9fdb69d84866db1afecad8
jessetusiime <jessetusiime078@gmail.com>
2025-10-31 17:39:37 +0300

    sort the list and print in ascending order

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,20 +1,17 @@
 using System;
 using System.Collections.Generic;
 
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         List<int> numbers = new List<int>();
         int userNumber = -1;
 
         while (userNumber != 0)
         {
-            Console.Write("Enter a number: ");
+            Console.Write("  Enter number: ");
             userNumber = int.Parse(Console.ReadLine());
 
             if (userNumber != 0)
@@ -24,18 +21,16 @@ class Program
         }
 
         int sum = 0;
-
         foreach (int number in numbers)
         {
             sum += number;
         }
-        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"  The sum is: {sum}");
 
         double average = (double)sum / numbers.Count;
-        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"  The average is: {average}");
 
         int max = numbers[0];
-
         foreach (int number in numbers)
         {
             if (number > max)
@@ -43,25 +38,33 @@ class Program
                 max = number;
             }
         }
-        Console.WriteLine($"The maximum number is {max}");
+        Console.WriteLine($"  The largest number is: {max}");
 
-        // Smallest Positive number
-        int smallPositiv = int.MaxValue;
+        // smallest positive number
+        int smallestPositive = int.MaxValue;
         foreach (int num in numbers)
         {
-            if (num > 0 && num < smallPositiv)
+            if (num > 0 && num < smallestPositive)
             {
-                smallPositiv = num;
+                smallestPositive = num;
             }
         }
 
-        if (smallPositiv == int.MaxValue)
+        if (smallestPositive == int.MaxValue)
         {
-            Console.WriteLine("No positive numbers were entered!");
+            Console.WriteLine("  No positive numbers were entered!");
         }
         else
         {
-            Console.Writegit ($"The smallest Positive number is: {smallPositiv}");
+            Console.WriteLine($"  The smallest positive number is: {smallestPositive}");
+        }
+
+        // sort and print the list
+        numbers.Sort();
+        Console.WriteLine("  The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"  {number}");
         }
     }
 }
